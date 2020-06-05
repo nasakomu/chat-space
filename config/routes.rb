521514a21c6971d53349ed31eb1de05c2580b73e
nasakomu <nasakomunasakomu@gmail.com>
2020-06-06 07:10:34 +0900

    create groups-controller and add "new" and "create" actions in the controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'groups/new'
   devise_for :users
   root to: "messages#index"
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create]
 end
