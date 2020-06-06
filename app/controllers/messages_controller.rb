2a632a58926f4f849f271ad492e7ab61bff7ed0c
nasakomu <nasakomunasakomu@gmail.com>
2020-06-06 12:30:46 +0900

    create "create"-action for MessagesController

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,13 @@
 class MessagesController < ApplicationController
   def index
   end
+
+  def create
+    Message.create(message_params)
+  end
+
+  private
+    def message_params
+      params.require(:message).permit(:content, :image).merge(group_id: params[:id], user_id: current_user.id)
+    end
 end
