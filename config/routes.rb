5ed9fc04580cefb8b404bf86537522aaabdca55a
nasakomu <nasakomunasakomu@gmail.com>
2020-06-06 09:44:18 +0900

    edit routes.rb about root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'groups/new'
   devise_for :users
-  root to: "messages#index"
+  root to: "groups#index"
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update]
 end
