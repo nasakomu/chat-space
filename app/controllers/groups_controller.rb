06b30b76fbc03872684cfb620374bc102be4e7c3
nasakomu <nasakomunasakomu@gmail.com>
2020-06-07 05:31:16 +0900

    edit update-action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -23,7 +23,7 @@ class GroupsController < ApplicationController
   def update
     @group = Group.find(params[:id])
     if Group.update(group_params)
-      redirect_to messages_path, notice: "グループを更新しました"
+      redirect_to group_messages_path(@group), notice: "グループを更新しました"
     else
       render :edit
     end
