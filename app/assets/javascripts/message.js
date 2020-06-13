77e21955f5105bde279b8abe26818c6e303a47a6
nasakomu <nasakomunasakomu@gmail.com>
2020-06-14 05:58:12 +0900

    edit message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,31 @@
 $(function(){
+  function buildHTML(message){
+    if (message.image) {
+      let html = `<div class="MainChatList">
+                    <div class="MainChatList__nameField">
+                      <p class="MainChatList__name">${message.user_name}</p>
+                      <p class="MainChatList__timeStamp">${message.created_at}</p>
+                    </div>
+                    <div class="MainChatList__messageBox">
+                      <p class="MainChatList__message">${message.content}</p>
+                      <img src="${message.image_url}" class="MainChatList__image">
+                    </div>
+                  </div>`
+      return html;
+    } else {
+      let html = `<div class="MainChatList">
+                    <div class="MainChatList__nameField">
+                      <p class="MainChatList__name">${message.user_name}</p>
+                      <p class="MainChatList__timeStamp">${message.created_at}</p>
+                    </div>
+                    <div class="MainChatList__messageBox">
+                      <p class="MainChatList__message">${message.content}</p>
+                    </div>
+                  </div>`
+      return html;
+    }
+  }
+
  $('.MainMessageForm').on('submit',function(e){
    e.preventDefault();
    let formData = new FormData(this);
@@ -10,6 +37,10 @@ $(function(){
     dataType: 'json',
     processData: false,
     contentType: false
+   })
+   .done(function(message){
+     let html = buildHTML(message);
+     console.log(html);
    })
  });
 });
