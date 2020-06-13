384d795f29836f5f72f67fa1beb16829c3536910
nasakomu <nasakomunasakomu@gmail.com>
2020-06-13 20:23:38 +0900

    edit message.js for sending request to controller

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,15 @@
+$(function(){
+ $('.MainMessageForm').on('submit',function(e){
+   e.preventDefault();
+   let formData = new FormData(this);
+   let url = $(this).attr('action');
+   $.ajax({
+    type: "POST",
+    url: url,
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+   })
+ });
+});
